f1e6968b85e998ae76a7e8bf6e0fbd73ace42d7a
Garvier <jasandoval@ug.uchile.cl>
2024-10-29 12:52:33 -0300

    Tercer y cuarto experimento

--- T2.cpp
+++ T2.cpp
@@ -12,92 +12,7 @@ struct Node {
 
     Node(int val) : value(val), left(nullptr), right(nullptr), parent(nullptr) {}
 };
-double f(int i, int N){
-    float x = 0.0;
-    for(int j = 0; j < N; j++){
-        x += 1/pow(i+1,2);
-    }
-    double C=1/x;
-    return C/pow(i+1,2);
-}
 
-void primerExperimento(int N, int M) {
-    ABB abb;
-    splayTree splay;
-    vector<int> N_values, M_values;
-
-    for (int i = 0; i < N; i++) {
-        N_values.push_back(rand());
-    }
-
-    int buscar = M / N;
-    for (int i = 0; i < M; i++) {
-        M_values.push_back(N_values[i % buscar]);
-    }
-
-    shuffle(M_values.begin(), M_values.end(), default_random_engine(0));
-
-    // Inserta los valores de 1 a N en ambos árboles
-    for (int i = 1; i <= N; i++) {
-        abb.insert(i);
-        splay.insert(i);
-    }
-
-    // Busca los valores de 1 a M en ambos árboles
-    for (int i = 1; i <= M; i++) {
-        abb.search(i);
-        splay.search(i);
-    }
-}
-
-void segundoExperimento(int N, int M){
-    ABB abb;
-    splayTree splay;
-    vector<int> N_values, B;
-    for(int i = 0; i < N; i++){
-        int x = rand();
-        N_values.push_back(x);
-        abb.insert(x);
-        splay.insert(x);
-    }
-    for(int i = 0; i < M; i++){
-        int x= floor(f(i,N)*N);
-        for(int j = 0; j < x; j++){
-            B.push_back(N_values[j]);
-        }
-    }
-    shuffle(B.begin(), B.end(), default_random_engine(0));
-    for(int i = 0; i < M; i++){
-        abb.search(B[i]);
-        splay.search(B[i]);
-    }
-}
-
-void tercerExperimento(int N, int M){
-    ABB abb;
-    splayTree splay;
-    vector<int> N_values, M_values;
-    for(int i = 0; i < N; i++){
-        int x = rand();
-        N_values.push_back(x);  
-    }
-    sort(N_values.begin(), N_values.end());
-    for(int i = 0; i < M; i++){
-        abb.insert(N_values[i]);
-        splay.insert(N_values[i]);
-    }
-    int buscar = M / N;
-    for (int i = 0; i < M; i++) {
-        M_values.push_back(N_values[i % buscar]);
-    }
-    shuffle(M_values.begin(), M_values.end(), default_random_engine(0));
-    for (int i = 1; i <= M; i++) {
-        abb.search(i);
-        splay.search(i);
-    }
-
-
-}
 
 
 class ABB{
@@ -295,9 +210,176 @@ class splayTree{
     }
 };
 
+
+double f(int i, int N) {
+    double sum = 0.0;
+    for (int j = 0; j < N; j++) {
+        sum += 1.0 / pow(j + 1, 2);
+    }
+    double C = 1.0 / sum;
+    return C / pow(i + 1, 2);
+}
+
+
+void primerExperimento(int N, int M) {
+    ABB abb;
+    splayTree splay;
+    vector<int> N_values, M_values;
+
+    // Generar N valores aleatorios y insertarlos en los árboles
+    for (int i = 0; i < N; i++) {
+        int x = rand();
+        N_values.push_back(x);
+        abb.insert(x);
+        splay.insert(x);
+    }
+
+    // Crear M/N copias de cada elemento y agregarlas a M_values
+    int copias = M / N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < copias; j++) {
+            M_values.push_back(N_values[i]);
+        }
+    }
+
+
+    int resto = M % N;
+    for (int i = 0; i < resto; i++) {
+        M_values.push_back(N_values[i]);
+    }
+
+    // Permutar aleatoriamente M_values
+    shuffle(M_values.begin(), M_values.end(), default_random_engine(0));
+
+    // Realizar las búsquedas
+    for (int i = 0; i < M_values.size(); i++) {
+        abb.search(M_values[i]);
+        splay.search(M_values[i]);
+    }
+}
+
+
+void segundoExperimento(int N, int M) {
+    ABB abb;
+    splayTree splay;
+    vector<int> N_values, B;
+
+    // Generar N valores aleatorios y insertarlos en los árboles
+    for (int i = 0; i < N; i++) {
+        int x = rand();
+        N_values.push_back(x);
+        abb.insert(x);
+        splay.insert(x);
+    }
+
+    // Crear el arreglo B utilizando la función de probabilidad f(i, N)
+    for (int i = 0; i < N; i++) {
+        double prob = f(i, N);
+        int s_i = floor(M * prob);
+        for (int j = 0; j < s_i; j++) {
+            B.push_back(N_values[i]);
+        }
+    }
+
+    // Permutar aleatoriamente B
+    shuffle(B.begin(), B.end(), default_random_engine(time(0)));
+
+    // Realizar las búsquedas
+    for (int i = 0; i < B.size(); i++) {
+        abb.search(B[i]);
+        splay.search(B[i]);
+    }
+}
+
+
+void tercerExperimento(int N, int M) {
+    ABB abb;
+    splayTree splay;
+    vector<int> N_values, M_values;
+
+    // Generar N valores aleatorios
+    for (int i = 0; i < N; i++) {
+        int x = rand();
+        N_values.push_back(x);
+    }
+
+    sort(N_values.begin(), N_values.end());
+
+
+    for (int i = 0; i < N; i++) {
+        abb.insert(N_values[i]);
+        splay.insert(N_values[i]);
+    }
+
+    int copias = M / N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < copias; j++) {
+            M_values.push_back(N_values[i]);
+        }
+    }
+
+    // Manejar el resto si M % N != 0
+    int resto = M % N;
+    for (int i = 0; i < resto; i++) {
+        M_values.push_back(N_values[i]);
+    }
+
+    shuffle(M_values.begin(), M_values.end(), default_random_engine(0));
+
+    // Realizar las búsquedas
+    for (int i = 0; i < M_values.size(); i++) {
+        abb.search(M_values[i]);
+        splay.search(M_values[i]);
+    }
+}
+
+
+void cuartoExperimento(int N, int M) {
+    ABB abb;
+    splayTree splay;
+    vector<int> A_values, C_values, M_values;
+
+    // Generar N valores aleatorios y almacenarlos en A_values
+    for (int i = 0; i < N; i++) {
+        int x = rand();
+        A_values.push_back(x);
+    }
+
+    // Copiar A_values a C_values y ordenar C_values
+    C_values = A_values;
+    sort(C_values.begin(), C_values.end());
+
+    // Insertar los elementos ordenados de C_values en los árboles
+    for (int i = 0; i < N; i++) {
+        abb.insert(C_values[i]);
+        splay.insert(C_values[i]);
+    }
+
+    // Crear M_values utilizando la función de probabilidad f(i, N) basada en A_values
+    for (int i = 0; i < N; i++) {
+        double prob = f(i, N);
+        int s_i = floor(M * prob);
+        for (int j = 0; j < s_i; j++) {
+            M_values.push_back(A_values[i]); // Usamos A_values para mantener el desorden
+        }
+    }
+
+    // Permutar aleatoriamente M_values
+    shuffle(M_values.begin(), M_values.end(), default_random_engine(time(0)));
+
+    // Realizar las búsquedas
+    for (int i = 0; i < M_values.size(); i++) {
+        abb.search(M_values[i]);
+        splay.search(M_values[i]);
+    }
+}
+
 int main() {
     // Ejecuta la experimentación en ambos árboles
+    // falta generar los N y el calculo de las ejecuciones de cada una
     primerExperimento(1000, 10000);
-
+    segundoExperimento(1000, 10000);
+    tercerExperimento(1000, 10000);
+    cuartoExperimento(1000, 10000);
     return 0;
 }
